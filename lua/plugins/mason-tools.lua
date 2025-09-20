fd17502fb64b0261a794c6e8e1f9d07795739faa
Riximus <riximuslol@gmail.com>
2025-09-20 15:38:40 +0200

    🚚 Renamed mason github path to mason-org

--- lua/plugins/mason-tools.lua
+++ lua/plugins/mason-tools.lua
@@ -1,13 +1,13 @@
 return {
-  "WhoIsSethDaniel/mason-tool-installer.nvim",
-  event = "VeryLazy",
-  dependencies = { "williamboman/mason.nvim" },
-  opts = function()
-    return {
-      ensure_installed = require("config.tools"),
-      run_on_start = true,
-      auto_update = false,
-      start_delay = 0, -- you can bump this if you want it to run after UI is up
-    }
-  end,
+	"WhoIsSethDaniel/mason-tool-installer.nvim",
+	event = "VeryLazy",
+	dependencies = { "mason-org/mason.nvim" },
+	opts = function()
+		return {
+			ensure_installed = require("config.tools"),
+			run_on_start = true,
+			auto_update = false,
+			start_delay = 0, -- you can bump this if you want it to run after UI is up
+		}
+	end,
 }
